71cf38e592c7f8dea6778b662e0033cf4f0779b9
CarlosEFPaiva <carlosedufpaiva@poli.ufrj.br>
2021-09-10 10:59:51 -0300

    implemented: downloaded Dayjs lib, already being used to adjust the date of 'TodaysHabits Screen' title.

--- src/pages/TodaysHabitsScreen/TodaysHabitsScreen.js
+++ src/pages/TodaysHabitsScreen/TodaysHabitsScreen.js
@@ -2,13 +2,14 @@ import Container from "../../shared/styles/Container";
 import HabitDueToday from "./components/HabitDueToday";
 import ScreenTitle from "../../shared/components/ScreenTitle";
 import ScreenSubtitle from "./components/ScreenSubtitle";
+import dayjs from "dayjs"
 
 export default function TodaysHabitsScreen({ setAreFixedBarsHidden }) {
     setAreFixedBarsHidden(false)
     const completionPercentage = 67;
     return (
         <Container backgroundColor = "#F2F2F2" horizontalPadding = "18px" topPadding = "92px" bottomPadding = "120px" >
-            <ScreenTitle text = "Segunda, 17/05" />
+            <ScreenTitle text = {StructuredTodaysDate()} />
             <ScreenSubtitle completionPercentage = {completionPercentage} />
 
             <HabitDueToday isHabitComplete = {false} title = {"Andar de bike"} />
@@ -17,4 +18,18 @@ export default function TodaysHabitsScreen({ setAreFixedBarsHidden }) {
 
         </Container>
     );
+}
+
+function StructuredTodaysDate() {
+    const WeekdayCodeCorrelation = [
+        {code: 0, weekday: "Domingo"},
+        {code: 1, weekday: "Segunda"},
+        {code: 2, weekday: "Terça"},
+        {code: 3, weekday: "Quarta"},
+        {code: 4, weekday: "Quinta"},
+        {code: 5, weekday: "Sexta"},
+        {code: 6, weekday: "Sábado"},
+    ]
+    const WeekdayName = WeekdayCodeCorrelation.find( ({ code }) => code === dayjs().day() ).weekday
+    return `${WeekdayName}, ${dayjs().format('DD/MM')}`
 }
